7cf61bc8f0b0d4b2916a16a02260ff5cd301acb9
Eugene Sergueev <Eugene Sergueev>
2016-05-04 20:24:27 +0300

    Add expression parser, naive example of sql where statement generator, unit tests

---
+++ src/Grammar/ExpressionParser.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text;
+using Sprache;
+
+namespace Grammar
+{
+    public static class ExpressionParser
+    {
+        private static readonly Parser<string> Field =
+            Sprache.Parse
+                .Identifier(Sprache.Parse
+                    .Letter, Sprache.Parse
+                        .LetterOrDigit
+                        .XOr(Sprache.Parse
+                            .Char('_'))
+                        .XOr(Sprache.Parse
+                            .Char('-')))
+                .Token();
+
+
+        private static readonly Parser<Operator> Equal = Operator(":=", Grammar.Operator.Equals)
+            .Or(Operator(":", Grammar.Operator.Equals))
+            .Or(Operator("=", Grammar.Operator.Equals));
+
+        private static readonly Parser<Operator> NotEqual = Operator("!=", Grammar.Operator.NotEqual)
+            .Or(Operator("<>", Grammar.Operator.NotEqual));
+
+        private static readonly Parser<Operator> GreaterThan = Operator(">", Grammar.Operator.GreaterThan);
+        private static readonly Parser<Operator> GreaterOrEqual = Operator(">=", Grammar.Operator.GreaterOrEqual);
+
+        private static readonly Parser<Operator> LessThan = Operator("<", Grammar.Operator.LessThan);
+        private static readonly Parser<Operator> LessOrEqual = Operator("<=", Grammar.Operator.LessOrEqual);
+
+        //Comporator: ':=' | ':' | '=' | '!=' | '<>' | '>=' | '<=' | '>' | '<' 
+        // NOTE: ORDER MATTERS, since otherwise shorter comporator will be recognized before longer ones!
+        private static readonly Parser<Operator> Comporator =
+            Equal
+                .Or(NotEqual)
+                .Or(LessOrEqual)
+                .Or(GreaterOrEqual)
+                .Or(GreaterThan)
+                .Or(LessThan)
+                .Named("Comporator");
+
+        private static readonly Parser<char> OpenParenthesis =
+            Sprache.Parse
+                .Char('(')
+                .Token();
+
+        private static readonly Parser<char> CloseParenthesis =
+            Sprache.Parse
+                .Char(')')
+                .Token();
+
+        private static readonly Parser<char> Negative =
+            Sprache.Parse
+                .Char('-');
+
+        public static readonly Parser<long> Number =
+            from sign in Negative.Optional()
+            from number in Sprache.Parse.Number
+            select long.Parse(sign.IsEmpty ? number : sign.Get() + number);
+
+        public static readonly Parser<string> Token =
+            Sprache.Parse
+                .LetterOrDigit
+                .AtLeastOnce()
+                .Text()
+                .Named(nameof(Token));
+
+        public static readonly Parser<string> QuotedString =
+            DelimitedBy('\'')
+                .Or(DelimitedBy('\"'))
+                .Named(nameof(QuotedString));
+
+        //Value: Number | Token | String
+        private static readonly Parser<TypedValue> Value =
+            Number.Select(n => new NumberValue(n))
+                .Or<TypedValue>(QuotedString.Select(s => new QuotedStringValue(s)))
+                .Or<TypedValue>(Token.Select(t => new TokenValue(t)))
+                .Token()
+                .Named(nameof(Value));
+
+        private static readonly Parser<Operator> Logic = Operator("and", Grammar.Operator.And)
+            .Or(Operator("or", Grammar.Operator.Or));
+
+        //term:
+        //  '(' expr ')' |
+        //  field COMPORATOR value
+        private static readonly Parser<Node> InnerExpression =
+            Sprache.Parse
+                .Ref(() => Expression)
+                .Contained(OpenParenthesis, CloseParenthesis)
+                .Select(n => new InnerNode(n))
+                .Named(nameof(InnerExpression));
+
+        private static readonly Parser<Node> Term =
+            (from id in Field
+                from comporator in Comporator
+                from value in Value
+                select new ComparisonNode(id, comporator, value))
+                .Or(InnerExpression)
+                .Token()
+                .Named(nameof(Term));
+
+
+        //expr:
+        //  term |
+        // expr LOGIC expr
+        private static readonly Parser<Node> Expression = Sprache.Parse.ChainOperator(Logic,
+            Term, (op, left, right) => new LogicNode(op, left, right));
+
+
+        public static Node Parse(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            return Expression.End().Parse(input);
+        }
+
+        private static Parser<Operator> Operator(string op, Operator opType)
+        {
+            return Sprache.Parse.IgnoreCase(op).Token().Return(opType);
+        }
+
+        private static Parser<string> DelimitedBy(char delimiter)
+        {
+            return Sprache.Parse
+                .CharExcept(delimiter)
+                .AtLeastOnce()
+                .Text()
+                .Contained(Sprache.Parse.Char(delimiter), Sprache.Parse.Char(delimiter));
+        }
+    }
+
+
+    public enum Operator
+    {
+        #region Comporator
+
+        Equals,
+        NotEqual,
+        GreaterThan,
+        LessThan,
+        LessOrEqual,
+        GreaterOrEqual,
+
+        #endregion
+
+        #region Logical
+
+        And,
+        Or
+
+        #endregion
+    }
+
+    public abstract class TypedValue
+    {
+    }
+
+    public abstract class TypedValue<T> : TypedValue
+    {
+        public T Value { get; }
+
+        protected TypedValue(T value)
+        {
+            Value = value;
+        }
+    }
+
+    public sealed class TokenValue : TypedValue<string>
+    {
+        public TokenValue(string value) : base(value)
+        {
+        }
+    }
+
+    public sealed class NumberValue : TypedValue<long>
+    {
+        public NumberValue(long value) : base(value)
+        {
+        }
+    }
+
+    public sealed class QuotedStringValue : TypedValue<string>
+    {
+        public QuotedStringValue(string value) : base(value)
+        {
+        }
+    }
+
+    public abstract class Node
+    {
+
+    }
+
+    public interface INodeVisitor
+    {
+        void Visit(LogicNode logicNode);
+        void Visit(InnerNode innerNode);
+        void Visit(ComparisonNode comparisonNode);
+    }
+
+    public class SqlNodeVisitor: INodeVisitor
+    {
+        private StringBuilder _writer;
+        private readonly string _space = " ";
+        public SqlNodeVisitor()
+        {
+            _writer = new StringBuilder();
+        }
+
+        public void Visit(LogicNode logicNode)
+        {
+            Visit((dynamic)logicNode.Left);
+            _writer.Append(_space).Append(OperatorToString(logicNode.Logic)).Append(_space);
+            Visit((dynamic)logicNode.Right);
+        }
+
+        public void Visit(InnerNode innerNode)
+        {
+            _writer.Append("(");
+            Visit((dynamic)innerNode.Inner);
+            _writer.Append(")");
+        }
+
+        public void Visit(ComparisonNode comparisonNode)
+        {
+            _writer.Append($"{comparisonNode.Field}{OperatorToString(comparisonNode.Comporator)}{GetValue(comparisonNode.Value)}");
+        }
+
+        private string OperatorToString(Operator op)
+        {
+            switch (op)
+            {
+                case Operator.Equals:
+                    return "=";
+                case Operator.NotEqual:
+                    return "<>";
+                case Operator.GreaterThan:
+                    return ">";
+                case Operator.LessThan:
+                    return "<";
+                case Operator.LessOrEqual:
+                    return "<=";
+                case Operator.GreaterOrEqual:
+                    return ">=";
+                case Operator.And:
+                    return "and";
+                case Operator.Or:
+                    return "or";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(op), op, null);
+            }
+        }
+
+        private object GetValue(TypedValue value)
+        {
+            var number =  value as NumberValue;
+            if (number != null)
+            {
+                return number.Value;
+            }
+
+            var token = value as TokenValue;
+            if (token != null)
+            {
+                return QuoteString(token.Value);
+            }
+
+            var quotedString = value as QuotedStringValue;
+            if (quotedString != null)
+            {
+                return QuoteString(quotedString.Value);
+            }
+
+            throw new ArgumentOutOfRangeException(nameof(value), value, null);
+        }
+
+        private string QuoteString(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return string.Empty;
+            }
+            string doubleQuote = "\"";
+            return string.Concat(doubleQuote, value, doubleQuote);
+        }
+
+        public override string ToString()
+        {
+            return $"{_writer}";
+        }
+    }
+
+    public sealed class InnerNode : Node
+    {
+        public Node Inner { get; }
+
+        public InnerNode(Node inner)
+        {
+            Inner = inner;
+        }
+    }
+
+    public sealed class LogicNode : Node
+    {
+        public Operator Logic { get; }
+        public Node Left { get; }
+        public Node Right { get; }
+
+        public LogicNode(Operator logic, Node left, Node right)
+        {
+            Logic = logic;
+            Left = left;
+            Right = right;
+        }
+    }
+
+    public sealed class ComparisonNode : Node
+    {
+        public string Field { get; }
+        public Operator Comporator { get; }
+        public TypedValue Value { get; }
+
+        public ComparisonNode(string field, Operator comporator, TypedValue value)
+        {
+            Field = field;
+            Comporator = comporator;
+            Value = value;
+        }
+    }
+}
---
+++ tests/Grammar.Tests/Parsing.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Grammar.Tests
+{
+    public class Parsing
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public Parsing(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void EmptyInputShouldThrowException(string input)
+        {
+            Assert.Throws<ArgumentNullException>(() => ExpressionParser.Parse(input));
+        }
+
+
+        [Theory]
+        [InlineData("(f1<1 and f2>\"2\") and ((f3<>token and f4:=to4e) or (f5>=1 and f6<= 10 ))")]
+        public void ShouldParseComplexExpression(string input)
+        {
+            //(inner_f1_f2) and (inner(inner_f3_f4 or inner_f5_f5) )
+            var sut = Assert.IsType<LogicNode>(ExpressionParser.Parse(input));
+
+            Assert.NotNull(sut);
+            Assert.Equal(Operator.And, sut.Logic);
+
+            //f1 and f2
+            var leftInner = Assert.IsType<InnerNode>(sut.Left);
+
+            var leftInnerLogical = Assert.IsType<LogicNode>(leftInner.Inner);
+            Assert.Equal(Operator.And, leftInnerLogical.Logic);
+
+            var f1ValueNode = Assert.IsType<ComparisonNode>(leftInnerLogical.Left);
+            Assert.Equal("f1", f1ValueNode.Field);
+            Assert.Equal(Operator.LessThan, f1ValueNode.Comporator);
+            var f1Value = Assert.IsType<NumberValue>(f1ValueNode.Value);
+            Assert.Equal(1, f1Value.Value);
+
+            var f2ValueNode = Assert.IsType<ComparisonNode>(leftInnerLogical.Right);
+            Assert.Equal("f2", f2ValueNode.Field);
+            Assert.Equal(Operator.GreaterThan, f2ValueNode.Comporator);
+            var f2Value = Assert.IsType<QuotedStringValue>(f2ValueNode.Value);
+            Assert.Equal("2", f2Value.Value);
+
+            //((inner_3_4) or (inner_5_6))
+            var rightInner = Assert.IsType<InnerNode>(sut.Right);
+            var rightInnerLogical = Assert.IsType<LogicNode>(rightInner.Inner);
+            Assert.Equal(Operator.Or, rightInnerLogical.Logic);
+
+            //f3 and f4
+            var f3F4Inner = Assert.IsType<InnerNode>(rightInnerLogical.Left);
+            var f3F4InnerLogical = Assert.IsType<LogicNode>(f3F4Inner.Inner);
+            Assert.Equal(Operator.And, f3F4InnerLogical.Logic);
+
+            var f3 = Assert.IsType<ComparisonNode>(f3F4InnerLogical.Left);
+            Assert.Equal("f3", f3.Field);
+            Assert.Equal(Operator.NotEqual, f3.Comporator);
+            var f3Value = Assert.IsType<TokenValue>(f3.Value);
+            Assert.Equal("token", f3Value.Value);
+
+            var f4 = Assert.IsType<ComparisonNode>(f3F4InnerLogical.Right);
+            Assert.Equal("f4", f4.Field);
+            Assert.Equal(Operator.Equals, f4.Comporator);
+            var f4Value = Assert.IsType<TokenValue>(f4.Value);
+            Assert.Equal("to4e", f4Value.Value);
+
+            //f5 and f6
+            var f5F5Inner = Assert.IsType<InnerNode>(rightInnerLogical.Right);
+            var f5F6InnerLogical = Assert.IsType<LogicNode>(f5F5Inner.Inner);
+            Assert.Equal(Operator.And, f5F6InnerLogical.Logic);
+
+            var f5 = Assert.IsType<ComparisonNode>(f5F6InnerLogical.Left);
+            Assert.Equal("f5", f5.Field);
+            Assert.Equal(Operator.GreaterOrEqual, f5.Comporator);
+            var f5Value = Assert.IsType<NumberValue>(f5.Value);
+            Assert.Equal(1, f5Value.Value);
+
+            var f6 = Assert.IsType<ComparisonNode>(f5F6InnerLogical.Right);
+            Assert.Equal("f6", f6.Field);
+            Assert.Equal(Operator.LessOrEqual, f6.Comporator);
+            var f6Value = Assert.IsType<NumberValue>(f6.Value);
+            Assert.Equal(10, f6Value.Value);
+        }
+
+        [Theory]
+        [InlineData("(f1<1 and f2>\"2\") and ((f3<>token and f4:=to4e) or (f5>=1 and f6<= 10 ))",
+            "(f1<1 and f2>\"2\") and ((f3<>\"token\" and f4=\"to4e\") or (f5>=1 and f6<=10))")]
+        [InlineData("f1 = 100", "f1=100")]
+        [InlineData("f1 : token", "f1=\"token\"")]
+        [InlineData("f1 :=\"quoted string \"", "f1=\"quoted string \"")]
+        [InlineData("f1 :=100 and f2  >10", "f1=100 and f2>10")]
+        [InlineData("f1 :=100 and (f2>tok3n)", "f1=100 and (f2>\"tok3n\")")]
+        public void SqlVisitorShouldGenerateProperOutput(string input, string output)
+        {
+            var node = ExpressionParser.Parse(input);
+            var sut = new SqlNodeVisitor();
+
+            sut.Visit((dynamic) node);
+
+            Assert.Equal(output, sut.ToString());
+        }
+
+        [Theory]
+
+        #region NumberValue
+
+        [InlineData("f1 = 100", "f1", Operator.Equals, 100L, typeof (NumberValue))]
+        [InlineData("f1 : 100", "f1", Operator.Equals, 100L, typeof (NumberValue))]
+        [InlineData("f1 := 100", "f1", Operator.Equals, 100L, typeof (NumberValue))]
+        [InlineData("f1 <> 100", "f1", Operator.NotEqual, 100L, typeof (NumberValue))]
+        [InlineData("f1 != 100", "f1", Operator.NotEqual, 100L, typeof (NumberValue))]
+        [InlineData("f1 > 100", "f1", Operator.GreaterThan, 100L, typeof (NumberValue))]
+        [InlineData("f1 >= 100", "f1", Operator.GreaterOrEqual, 100L, typeof (NumberValue))]
+        [InlineData("f1 < 100", "f1", Operator.LessThan, 100L, typeof (NumberValue))]
+        [InlineData("f1 <= 100", "f1", Operator.LessOrEqual, 100L, typeof (NumberValue))]
+
+        #endregion
+
+        #region TokenValue
+
+        [InlineData("f1 = tok3n", "f1", Operator.Equals, "tok3n", typeof (TokenValue))]
+        [InlineData("f1 : tok3n", "f1", Operator.Equals, "tok3n", typeof (TokenValue))]
+        [InlineData("f1 := tok3n", "f1", Operator.Equals, "tok3n", typeof (TokenValue))]
+        [InlineData("f1 <> tok3n", "f1", Operator.NotEqual, "tok3n", typeof (TokenValue))]
+        [InlineData("f1 != tok3n", "f1", Operator.NotEqual, "tok3n", typeof (TokenValue))]
+        [InlineData("f1 > tok3n", "f1", Operator.GreaterThan, "tok3n", typeof (TokenValue))]
+        [InlineData("f1 >= tok3n", "f1", Operator.GreaterOrEqual, "tok3n", typeof (TokenValue))]
+        [InlineData("f1 < tok3n", "f1", Operator.LessThan, "tok3n", typeof (TokenValue))]
+        [InlineData("f1 <= tok3n", "f1", Operator.LessOrEqual, "tok3n", typeof (TokenValue))]
+
+        #endregion
+
+        #region QuotedStringValue
+
+        [InlineData("f1 = \"quoted string\"", "f1", Operator.Equals, "quoted string", typeof (QuotedStringValue))]
+        [InlineData("f1 : \"quoted string\"", "f1", Operator.Equals, "quoted string", typeof (QuotedStringValue))]
+        [InlineData("f1 := \"quoted string\"", "f1", Operator.Equals, "quoted string", typeof (QuotedStringValue))]
+        [InlineData("f1 <> \"quoted string\"", "f1", Operator.NotEqual, "quoted string", typeof (QuotedStringValue))]
+        [InlineData("f1 != \"quoted string\"", "f1", Operator.NotEqual, "quoted string", typeof (QuotedStringValue))]
+        [InlineData("f1 > \"quoted string\"", "f1", Operator.GreaterThan, "quoted string", typeof (QuotedStringValue))]
+        [InlineData("f1 >= \"quoted string\"", "f1", Operator.GreaterOrEqual, "quoted string",
+            typeof (QuotedStringValue))]
+        [InlineData("f1 < \"quoted string\"", "f1", Operator.LessThan, "quoted string", typeof (QuotedStringValue))]
+        [InlineData("f1 <= \"quoted string\"", "f1", Operator.LessOrEqual, "quoted string", typeof (QuotedStringValue))]
+
+        #endregion
+
+        public void ShouldParseSimpleExpression(string input, string field, Operator op, object val, Type type)
+        {
+            var sut = Assert.IsType<ComparisonNode>(ExpressionParser.Parse(input));
+
+            Assert.NotNull(sut);
+            Assert.Equal(field, sut.Field);
+            Assert.Equal(op, sut.Comporator);
+            Assert.IsType(type, sut.Value);
+            dynamic value = Convert.ChangeType(sut.Value, type);
+            Assert.Equal(val, value.Value);
+        }
+    }
+}
